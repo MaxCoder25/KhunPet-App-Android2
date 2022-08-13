be9994a3b0abe38ae4dd69cf525cd7208889242f
DDe3 <Leonardo.Salinas@intisoluciones.com>
2022-08-13 13:49:09 -0500

    Implementacion de navegacion

--- app/src/main/java/com/example/khunpet/controllers/view_models/MainActivityViewModel.kt
+++ app/src/main/java/com/example/khunpet/controllers/view_models/MainActivityViewModel.kt
@@ -1,7 +1,47 @@
 package com.example.khunpet.controllers.view_models
 
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.khunpet.R
+import com.example.khunpet.ui.fragments.HomeFragment
+import com.example.khunpet.ui.fragments.LostAndFoundFragment
 
 class MainActivityViewModel: ViewModel() {
+
+    private val lostFoundFragment = LostAndFoundFragment()
+    private val homeFragment = HomeFragment()
+
+    val currentFragment : MutableLiveData<Fragment> by lazy {
+        MutableLiveData<Fragment>()
+    }
+    val currentTab : MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+
+    init {
+        currentFragment.postValue(homeFragment)
+        currentTab.postValue(1)
+    }
+
+    fun changeFragment(index:Int) : Boolean {
+        changeTab(index)
+        return when(index) {
+            R.id.home_button -> {
+                currentFragment.postValue(homeFragment)
+                true
+            }
+            R.id.lost_found_button -> {
+                currentFragment.postValue(lostFoundFragment)
+                true
+            }
+            else -> false
+        }
+    }
+
+    fun changeTab(index:Int) {
+        currentTab.postValue(index)
+    }
+
 
 }
--- app/src/main/java/com/example/khunpet/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/khunpet/ui/activities/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.khunpet.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.example.khunpet.R
 import com.example.khunpet.controllers.view_models.MainActivityViewModel
 import com.example.khunpet.databinding.ActivityMainBinding
@@ -18,5 +20,36 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        binding.navBar.setOnItemSelectedListener {
+            viewModel.changeFragment(it.itemId)
+        }
+
+        viewModel.currentFragment.observe(this, Observer {
+            cambiarFragmento(it)
+        })
+
+        viewModel.currentTab.observe(this, Observer {
+            binding.navBar.selectedItemId = it
+        })
+
+
     }
+
+    override fun onBackPressed() {
+        if (viewModel.currentTab.value == R.id.home_button) {
+            super.onBackPressed()
+        } else {
+            viewModel.changeFragment(R.id.home_button)
+        }
+    }
+
+    fun cambiarFragmento(fragmento : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction
+            .replace(binding.fragmentContainer.id, fragmento)
+            .commit()
+    }
+
+
 }
